56bbc1479df76a4899afe6f468520039d2c2f10e
Alex <146895225+AcidGh0st@users.noreply.github.com>
2024-04-17 17:06:39 -0600

    Add files via upload

---
+++ Laboratorio5.Algoritmos/Laboratory4.Algoritmos/IF-3001.021-Lab4-master/src/main/java/controller/AddOdersController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AddOdersController
+{
+    @javafx.fxml.FXML
+    private TextField IDtestField;
+    @javafx.fxml.FXML
+    private TextField quantityTestField;
+    @javafx.fxml.FXML
+    private TextField UPriceText;
+    @javafx.fxml.FXML
+    private TextField totalPriceText;
+    @javafx.fxml.FXML
+    private ChoiceBox choiceBox;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private Button addButton;
+    @javafx.fxml.FXML
+    private Button cleanButton;
+    @javafx.fxml.FXML
+    private Button closeButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cleanAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void closeAction(ActionEvent actionEvent) {
+    }
+}
---
+++ Laboratorio5.Algoritmos/Laboratory4.Algoritmos/IF-3001.021-Lab4-master/src/main/java/controller/OrdesCircularLinkedController.java
@@ -0,0 +1,80 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class OrdesCircularLinkedController
+{
+    @javafx.fxml.FXML
+    private TableColumn idTable;
+    @javafx.fxml.FXML
+    private TableColumn dateTable;
+    @javafx.fxml.FXML
+    private TableColumn productTable;
+    @javafx.fxml.FXML
+    private TableColumn quantityTable;
+    @javafx.fxml.FXML
+    private TableColumn priceTable;
+    @javafx.fxml.FXML
+    private Button addButton;
+    @javafx.fxml.FXML
+    private Button sortIDButton;
+    @javafx.fxml.FXML
+    private Button sortName;
+    @javafx.fxml.FXML
+    private Button containsButton;
+    @javafx.fxml.FXML
+    private Button sizeButton;
+    @javafx.fxml.FXML
+    private Button getPrevTable;
+    @javafx.fxml.FXML
+    private Button getNextButton;
+    @javafx.fxml.FXML
+    private Button removeButton;
+    @javafx.fxml.FXML
+    private Button removeFirtsButton;
+    @javafx.fxml.FXML
+    private Button clearButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortyNameAction(ActionEvent actionEvent) {
+    }
+
+
+    @javafx.fxml.FXML
+    public void containsAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sizeAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getPrevAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getNextAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeFirtsAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearAction(ActionEvent actionEvent) {
+    }
+}
